9a379d23858ad75882403bf3fd847c5e65cc122b
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2019-02-22 00:00:52 +0100

    update drawing of all triangles. it all works now. Next step is uv's

--- Assets/GenerateFaces.cs
+++ Assets/GenerateFaces.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class GenerateFaces : MonoBehaviour {
 
+    public bool showVerteces;
     public Mesh mesh;
     public int resolution;
     public float height, radius;
     Vector3[] vertexArray;
+    int trisPerVerts = 12;
 
     void Start()
     {
+        showVerteces = true;
         //RebuildMesh();
     }
 
@@ -18,10 +21,10 @@ public class GenerateFaces : MonoBehaviour {
     {
         mesh = new Mesh();
 
-        resolution = Mathf.Clamp(resolution, 3, 400);
+        resolution = Mathf.Clamp(resolution, 1, 400);
 
         vertexArray = new Vector3[2 * (1 + resolution)];
-        int[] triangles = new int[resolution * 4 * 3];//fix number overflow (initialization of int can be a negative number, as is now)
+        int[] triangles = new int[resolution * trisPerVerts];//fix number overflow (initialization of int can be a negative number, as is now)
 
         vertexArray[2 * (1 + resolution) - 1] = new Vector3(0, .5f * height, 0);
         vertexArray[2 * (1 + resolution) - 2] = new Vector3(0, -.5f * height, 0);
@@ -32,6 +35,52 @@ public class GenerateFaces : MonoBehaviour {
             float cornerAngle = (float)i / resolution * 2 * Mathf.PI;
             vertexArray[i] = new Vector3(Mathf.Cos(cornerAngle) * radius, .5f * height, Mathf.Sin(cornerAngle) * radius);
             vertexArray[i + resolution] = new Vector3(Mathf.Cos(cornerAngle) * radius, -.5f * height, Mathf.Sin(cornerAngle) * radius);
+        }
+
+        //Debug.Log("triangleCount: " + triangles.Length + ", vertexCount: " + vertexArray.Length + ", resolution: " + resolution + ".");
+
+        for (int triIndex = 0, vertIndex = 0; triIndex < triangles.Length && vertIndex < vertexArray.Length; triIndex += trisPerVerts, vertIndex++)
+        {
+            if (triIndex == (resolution - 1) * trisPerVerts)//if we're at the last vertex of the circle
+            {
+                //build a triangle on the top circle from the last vertex on that circle
+                triangles[triIndex] = vertIndex;
+                triangles[triIndex + 1] = 2 * (1 + resolution) - 1;
+                triangles[triIndex + 2] = 0;
+
+                //build a triangle on the bottom circle from the last vertex on that circle
+                triangles[triIndex + 3] = vertIndex + resolution;
+                triangles[triIndex + 4] = resolution;
+                triangles[triIndex + 5] = 2 * (1 + resolution) - 2;
+
+                //build quad on the side from the last vertices of the circles
+                triangles[triIndex + 6] = vertIndex;
+                triangles[triIndex + 7] = 0;
+                triangles[triIndex + 8] = resolution;
+                triangles[triIndex + 9] = vertIndex;
+                triangles[triIndex + 10] = resolution;
+                triangles[triIndex + 11] = vertIndex + resolution;
+            }
+            else
+            {
+                //build a triangle on the top circle
+                triangles[triIndex] = vertIndex;
+                triangles[triIndex + 1] = 2 * (1 + resolution) - 1;
+                triangles[triIndex + 2] = vertIndex + 1;
+
+                //build a triangle on the bottom circle
+                triangles[triIndex + 3] = vertIndex + resolution;
+                triangles[triIndex + 4] = vertIndex + resolution + 1;
+                triangles[triIndex + 5] = 2 * (1 + resolution) - 2;
+
+                //build quad on the side
+                triangles[triIndex + 6] = vertIndex;
+                triangles[triIndex + 7] = vertIndex + 1;
+                triangles[triIndex + 8] = vertIndex + resolution + 1;
+                triangles[triIndex + 9] = vertIndex;
+                triangles[triIndex + 10] = vertIndex + resolution + 1;
+                triangles[triIndex + 11] = vertIndex + resolution;
+            }
         }
 
         /*
@@ -79,10 +128,12 @@ public class GenerateFaces : MonoBehaviour {
     private void OnDrawGizmos()//the ggizmo only displays the vertices of the local mesh variable and not 'GetComponent<MeshFilter>().mesh'
     {
         Gizmos.color = Color.yellow;
-
-        for (int i = 0; i < mesh.vertices.Length; i++)
+        if (showVerteces)
         {
-            Gizmos.DrawSphere(mesh.vertices[i], .2f);
+            for (int i = 0; i < mesh.vertices.Length; i++)
+            {
+                Gizmos.DrawSphere(mesh.vertices[i], .1f);
+            }
         }
     }
 }
